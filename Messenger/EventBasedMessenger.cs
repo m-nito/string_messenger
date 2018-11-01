36dd77ec3da94ae3eede0a91aa525209c497341a
m-nito <masamisce@gmail.com>
2018-11-02 02:02:22 +0900

    EventHandlerImpl.

---
+++ Messenger/EventBasedMessenger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messenger
+{
+    public delegate void TheEventHandler<T>(T value);
+
+    public class EventBased {
+        public static event TheEventHandler<string> TheHandler;
+        public static void Call(string str) {
+            TheHandler(str);
+        }
+    }
+    public class EventBasedExec {
+        public void Show() {
+            EventBased.TheHandler += GUIMethod;
+            EventBased.Call("aa");
+        }
+        private void GUIMethod(string str) {
+            Console.WriteLine(str);
+        }
+    }
+}
